42cfbf83540280e7938d5cbe219cbef687015d07
Olga <genntanaka@mail.ru>
2024-07-27 18:56:40 +0700

    Добавялет возможность изменять размер и эффект для изображения

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 import {isEscapeKey} from './helpers.js';
-import './change-size-image.js';
-//import {pristine} from './validation-form.js';
+import {resizeLoadImage} from './change-size-image.js';
+import {changeEffect} from './image-effet.js';
 
 const imageLoadButton = document.querySelector('#upload-file');
 const imageLoadModal = document.querySelector('.img-upload__overlay');
@@ -8,6 +8,11 @@ const loadForm = document.querySelector('#upload-select-image');
 const imageLoadPreview = loadForm.querySelector('.img-upload__preview img');
 const effectsImage = loadForm.querySelectorAll('.effects__item .effects__preview');
 const closeLoadModalButton = imageLoadModal.querySelector('.img-upload__cancel');
+
+const minusSizeButton = loadForm.querySelector('.scale__control--smaller');
+const plusSizeButton = loadForm.querySelector('.scale__control--bigger');
+
+const imageEffectsList = loadForm.querySelector('.effects__list');
 
 const onModalEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -23,6 +28,20 @@ function openLoadModal(){
   document.addEventListener('keydown', onModalEscKeydown);
 
   // события на кнопки изменения размера
+  minusSizeButton.addEventListener('click', (evt) => {
+    resizeLoadImage(evt);
+  });
+  plusSizeButton.addEventListener('click', (evt) => {
+    resizeLoadImage(evt);
+  });
+
+  //выбор эффекта для загруженой фотографии
+  imageEffectsList.addEventListener('click', (evt) => {
+    const target = evt.target.closest('.effects__radio');
+    if (target) {
+      changeEffect(target);
+    }
+  });
 }
 
 function closeLoadModal(){
@@ -31,6 +50,11 @@ function closeLoadModal(){
   imageLoadModal.classList.add('hidden');
   document.removeEventListener('keydown', onModalEscKeydown);
   closeLoadModalButton.removeEventListener('click', closeLoadModal);
+
+  // события на кнопки изменения размера
+  minusSizeButton.removeEventListener('click', resizeLoadImage);
+  plusSizeButton.removeEventListener('click', resizeLoadImage);
+
 }
 
 function onPreviewImageLoadModal (evt) {
